c69bb1c10c203de60f0b158034074a0870f9085b
unknown <kaburuedwin123@gmail.com>
2020-12-16 20:04:32 -0800

    Interview Questions Edwin Kaburu

---
+++ Models/DistinctQuarterCode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewAssignment.Models
+{
+    public class DistinctQuarterCode
+    {
+        // Private List Field of Classes : Only accessible within this Class
+        private List<Classes> _classes = new List<Classes>();
+        // Private label regarding the quarter code : Only accessible within this Class
+        private string _label;
+
+        public DistinctQuarterCode() { }
+
+        public DistinctQuarterCode(string user_label)
+        {
+            _label = user_label;
+        }
+
+        public DistinctQuarterCode(string user_label, List<Classes> user_classes)
+        {
+            _label = user_label;
+            _classes = user_classes;
+        }
+
+        // Get Accessor for _classes
+        public List<Classes> Classes
+        {
+            get { return _classes; }
+        }
+
+        // Get and Setter Accessor for label
+        // Could be C012, C013 or C011
+        public string label
+        {
+            get { return _label; }
+
+            set { _label = value; }
+        }
+
+        // Insert a Class object to _classes
+        public void Insert(Classes new_classes)
+        {
+            _classes.Add(new_classes);
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using InterviewAssignment.Models;
+using System.Collections;
+
+namespace InterviewAssignment.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        // Private Instance of List of DistinctQuarterCodes.
+        // See class at Models/DistinctQuarterCode 
+        private List<DistinctQuarterCode> _distinctQuarterCodes = new List<DistinctQuarterCode>();
+
+        public List<Classes> _classes { get; set; }
+
+        // Get Accessor for _distinctQuarterCodes
+        public List<DistinctQuarterCode> DistinctQuarterCodes
+        {
+            get { return SplitQuarterCode(); }
+
+        }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "ClassData.csv");
+
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                _classes = csv.GetRecords<Classes>().ToList();
+
+            }
+        }
+
+        private List<DistinctQuarterCode> SplitQuarterCode()
+        {
+            // Create Three Instances of DistinctQuarterCode for Distinct Quarter Code
+            DistinctQuarterCode _distcode_11 = new DistinctQuarterCode("C011");
+            DistinctQuarterCode _distcode_12 = new DistinctQuarterCode("C012");
+            DistinctQuarterCode _distcode_13 = new DistinctQuarterCode("C013");
+
+            // _distinctQuarterCodes = new List<DistinctQuarterCode>();
+
+            foreach (var items in _classes)
+            {
+                // Starting Index
+                int begin_index = items.ClassID.Length - 4;
+
+                // Substring 
+                string code_substring = items.ClassID.Substring(begin_index);
+
+                switch (code_substring)
+                {
+                    case "C011":
+                        _distcode_11.Insert(items);
+                        break;
+                    case "C012":
+                        _distcode_12.Insert(items);
+                        break;
+                    default:
+                        _distcode_13.Insert(items);
+                        break;
+                }
+            }
+
+            _distinctQuarterCodes.Add(_distcode_11);
+            _distinctQuarterCodes.Add(_distcode_12);
+            _distinctQuarterCodes.Add(_distcode_13);
+
+
+            return _distinctQuarterCodes.OrderByDescending(l => l.label).ToList();
+
+            //return _distinctQuarterCodes.OrderByDescending(l => l.label).ThenBy(sp => sp.Classes.OrderBy(sps => sps.CourseID)).ToList();
+
+        }
+
+    }
+}
